302a748739b8fb5875d8cc7c90e40882f3024967
didapax <alfonsi.acosta@gmail.com>
2024-07-10 09:58:02 -0400

    Update backed.py

--- backed.py
+++ backed.py
@@ -1,28 +1,46 @@
 import json
 from flask import Flask, request, jsonify
-
+from bson.objectid import ObjectId
+from bson import json_util
 app = Flask(__name__)
+
+headers = {"Content-Type": "application/json"}
+
+@app.before_request
+def before_request():
+    # Abre la conexión antes de cada solicitud
+
+
+@app.teardown_request
+def teardown_request(exception=None):
+    # Cierra la conexión después de cada solicitud
+
+        
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_requests():
     try:
         if request.method == 'GET':
-            valor = request.args.get('saludar', '')
-            valor = f"Hola Mundo Get {valor}"
+            accion = request.args.get('accion', '')            
                 
-            # Crear un diccionario con la respuesta JSON
-            response_data = {'mensaje': f"Respuesta para GET con datos: {valor}"}
+            if accion == "listaof":
+                #aqui va la logica para esta accion GET
+            elif accion == "lista":
+                #otra
+            else:
+                return jsonify({'message': 'No hay datos'})                
 
-            # Devolver la respuesta como JSON
-            return jsonify(response_data)
         elif request.method == 'POST':
-            data = request.form.get('campo')
-
-            # Crear un diccionario con la respuesta JSON
-            response_data = {'mensaje': f"Respuesta para POST con datos: {data}"}
-
-            # Devolver la respuesta como JSON
-            return jsonify(response_data)
+            data = request.json
+            accion = data.get('accion','')
+            if accion == "insert":
+                #aqio va la logica para el POSt inser
+            elif accion == "update":
+                #otra
+            else:
+                return jsonify({'error': 'Tipo de operación no válida'}), 400
+        else
+            return jsonify({'error': 'No encuentra la página o recurso solicitado'}), 404                
     except Exception as e:
         # Manejar cualquier excepción aquí
         error_message = f"Error: {str(e)}"
